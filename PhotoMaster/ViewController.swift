8057f7fde6da89698c157cb264703ee5730eebb1
IsseTommy <0jofePfN>
2019-04-11 10:36:42 +0900

    Initial Commit

---
+++ PhotoMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoMaster
+//
+//  Created by Tommy on 2019/04/11.
+//  Copyright © 2019 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
